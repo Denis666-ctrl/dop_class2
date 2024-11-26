cd19be1c4c36b9ec24b7c4a8e255570ba7c782d1
Denis666-ctrl <ddneprovskij02@gmail.com>
2024-11-26 23:01:02 +0900

    Initial commit

---
+++ dop_class2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Calculator
+{
+    public static int method(int n)
+    {
+        int difference = Math.Abs(n - 123);
+
+        if (n > 123)
+        {
+            return difference * 3;
+        }
+
+        return difference;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите целое число n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = method(n);
+
+        Console.WriteLine($"Результат: {result}");
+    }
+}
